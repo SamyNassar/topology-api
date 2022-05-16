df0a5617f9d29c0d13e00dcbbee2ef042a0390a5
Samy Nassar <samynassar996@gmail.com>
2022-05-16 18:27:48 +0200

    Initial commit

---
+++ src/main/java/org/topology/api/Main.java
@@ -0,0 +1,14 @@
+package org.topology.api;
+
+import org.topology.api.model.Topology;
+import org.topology.api.model.utl.Utl;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
